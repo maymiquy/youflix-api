eec13a7a0688d92cc1ee9c62e162a4f602457363
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-03-16 21:12:46 +0700

    chore: add jwt strategy

---
+++ src/libs/jwt/jwt.strategy.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { jwtSecret } from 'src/utils/constant';
+import { Request } from 'express';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        JwtStrategy.extractJWT,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: jwtSecret,
+    });
+  }
+
+  private static extractJWT(req: Request): string | null {
+    if (req.cookies && 'token' in req.cookies) {
+      return req.cookies.token;
+    }
+    return null;
+  }
+
+  async validate(payload: { id: string; email: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.id,
+        email: payload.email,
+      },
+    });
+
+    return user;
+  }
+}
